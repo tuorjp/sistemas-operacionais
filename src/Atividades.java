7cff8e7733702e4f4892099b5178af32b536000b
JP <jplmarotinho22@gmail.com>
2025-04-10 14:04:22 -0300

    Atividades complementares, simulando as abordagens descritas em Sistemas Operacionais

---
+++ src/Atividades.java
@@ -0,0 +1,13 @@
+
+public class Atividades {
+    public static void main(String[] args) {
+        System.out.println("Selecione uma simulação: \n");
+        System.out.println(
+                """
+                1 - Execução monoprogramada\s
+                2 - Execução multiprogramada\s
+                3 - Simulação de interrupções de um processo\s
+                """
+        );
+    }
+}
